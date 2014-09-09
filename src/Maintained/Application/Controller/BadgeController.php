984ab8139fc4776301c1ff3719bfff54ae4cec61
Matthieu Napoli <matthieu@mnapoli.fr>
2014-09-09 11:06:59 +0700

    Error handling when reaching GitHub's API limit

--- src/Maintained/Application/Controller/BadgeController.php
+++ src/Maintained/Application/Controller/BadgeController.php
@@ -2,16 +2,34 @@
 
 namespace Maintained\Application\Controller;
 
+use Github\Exception\ApiLimitExceedException;
 use Maintained\Badge\BadgeProvider;
+use PUGX\Poser\Poser;
 
 /**
  * @author Matthieu Napoli <matthieu@mnapoli.fr>
  */
 class BadgeController
 {
-    public function __invoke($user, $repository, BadgeProvider $badgeProvider)
+    /**
+     * @Inject
+     * @var BadgeProvider
+     */
+    private $badgeProvider;
+
+    /**
+     * @Inject
+     * @var Poser
+     */
+    private $poser;
+
+    public function __invoke($user, $repository)
     {
-        $badge = $badgeProvider->getResolutionBadge($user, $repository);
+        try {
+            $badge = $this->badgeProvider->getResolutionBadge($user, $repository);
+        } catch (ApiLimitExceedException $e) {
+            $badge = $this->poser->generate('github-api', 'limit', '9C3838', 'svg');
+        }
 
         header('Content-type: image/svg+xml');
         echo $badge;
