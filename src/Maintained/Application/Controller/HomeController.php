28e497a61adb3bcecb1cd1026be796e1f3b69eff
Matthieu Napoli <matthieu@mnapoli.fr>
2014-09-17 22:38:16 +1200

    Added Piwik in the home

--- src/Maintained/Application/Controller/HomeController.php
+++ src/Maintained/Application/Controller/HomeController.php
@@ -12,18 +12,18 @@ class HomeController
     public function __invoke(Twig_Environment $twig)
     {
         $demoProjects = [
-            'symfony/symfony'           => 'Symfony 2',
-            'zendframework/zf2'         => 'Zend Framework 2',
-            'zendframework/zf1'         => 'Zend Framework 1',
-            'codeguy/Slim'              => 'Slim',
-            'silexphp/Silex'            => 'Silex',
-            'sebastianbergmann/phpunit' => 'PHPUnit',
-            'auraphp/Aura.Sql'          => 'Aura.Sql',
-            'Behat/Behat'               => 'Behat',
-            'schmittjoh/serializer'     => 'Serializer',
-            'kriswallsmith/assetic'     => 'Assetic',
-            'thephpleague/flysystem'    => 'Flysystem',
-            'guzzle/guzzle'             => 'Guzzle',
+            'symfony/symfony'        => 'Symfony 2',
+            'zendframework/zf2'      => 'Zend Framework 2',
+            'zendframework/zf1'      => 'Zend Framework 1',
+            'codeguy/Slim'           => 'Slim',
+            'silexphp/Silex'         => 'Silex',
+            'piwik/piwik'            => 'Piwik',
+            'auraphp/Aura.Sql'       => 'Aura.Sql',
+            'Behat/Behat'            => 'Behat',
+            'schmittjoh/serializer'  => 'Serializer',
+            'kriswallsmith/assetic'  => 'Assetic',
+            'thephpleague/flysystem' => 'Flysystem',
+            'guzzle/guzzle'          => 'Guzzle',
         ];
 
         echo $twig->render('home.twig', [
