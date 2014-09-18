353a0ec4a196323f25a0c3015fea830125c90ad0
Matthieu Napoli <matthieu@mnapoli.fr>
2014-09-19 11:18:58 +1200

    #8 set the cache headers for correct caching via CDN

--- src/Maintained/Application/Controller/BadgeController.php
+++ src/Maintained/Application/Controller/BadgeController.php
@@ -55,7 +55,10 @@ class BadgeController
             }
         }
 
+        // Cache the badge for 1 day
+        header('Cache-Control: max-age=86400');
         header('Content-type: image/svg+xml');
+
         echo $badge;
     }
 
