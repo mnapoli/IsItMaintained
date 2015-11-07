35d6c5f5fa612cb882304324c4c3a4f72e6b4cea
Matthieu Napoli <matthieu@mnapoli.fr>
2015-11-07 20:35:29 +0100

    Better support for PHP's built-in webserver

--- web/index.php
+++ web/index.php
@@ -15,6 +15,10 @@ use function Stratify\Framework\pipe;
 use function Stratify\Framework\router;
 use function Stratify\Router\route;
 
+if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']))) {
+    return false;
+}
+
 require __DIR__ . '/../vendor/autoload.php';
 
 $modules = [
